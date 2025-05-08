c3a34acc9ac6c135c6a5de5895d5c0d1c43c787c
subha <dassubhajit579@gmail.com>
2025-05-08 15:29:29 +0530

    Init commit

---
+++ mymath.py
@@ -0,0 +1,14 @@
+# A simple custom module 
+
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
---
+++ r_intro.R
@@ -0,0 +1,109 @@
+
+# Packages are codes (similar to python module) that add new functions to R
+# Base packages are already installed, but they need to be loaded
+# There are some third party packages, that needs to be installed to use.
+# We can get more packages from CRAN (https://cran.r-project.org/)
+# To install a R package you can run the r code
+# install.packages("package_name")
+# To load the packages in r to use, run
+# library(package_name)
+
+library(datasets)
+head(iris)
+summary(iris)
+
+plot(iris$Petal.Length)
+plot(iris$Species,iris$Petal.Width)
+plot(iris$Petal.Length,iris$Petal.Width)
+plot(iris)
+
+# plot with customization
+plot(iris$Petal.Length,iris$Petal.Width,col='red',pch=20,main='petal length vs petal width',
+     xlab='Petal Length', ylab='Petal Width')
+
+### Histogram
+hist(iris$Sepal.Length)
+hist(iris$Sepal.Width)
+
+# histogram for each species using customization
+par(mfrow=c(3,1))
+hist(iris$Petal.Width[iris$Species=='setosa'], xlim=c(0,3), breaks=9, main='Setosa',
+     xlab='', col='red')
+
+hist(iris$Petal.Width[iris$Species=='versicolor'], xlim=c(0,3), breaks=9, main='Versicolor',
+     xlab='', col='purple')
+
+hist(iris$Petal.Width[iris$Species=='virginica'], xlim=c(0,3), breaks=9, main='Virginica',
+     xlab='', col='green')
+
+par(mfrow=c(1,1))
+
+## barplots 
+head(mtcars)
+cylinder= table(mtcars$cyl)
+barplot(cylinder)
+
+### more plots in one figure
+?lynx
+head(lynx)
+
+hist(lynx, breaks=14, freq=FALSE, col='blue',main='Lynx data',
+     xlab='Number of Lynx Trapped')
+# add a normal distribution
+curve(dnorm(x,mean=mean(lynx),sd=sd(lynx)), col='black',lwd=2, add=TRUE)
+# add kernel density estimates
+lines(density(lynx), col='red',lwd=2)
+
+####### DATA TYPES
+n1 <- 15
+typeof(n1)
+
+n2 <- 1.5
+typeof(n2)
+
+c1 <- 'c'
+typeof(c1)
+
+c2 <- 'a string'
+typeof(c2)
+
+l1 <- TRUE
+typeof(l1)
+
+### vectors and basic operations
+v= c(10,20,30,40)
+is.vector(v)
+mean(v)
+sum(v)
+length(v)
+u=v[v>20]
+print(u)
+
+## matrix
+m1=matrix(c(1,2,3,4), nrow=2,byrow=TRUE)
+
+## data frame
+vNumeric <- c(1,2,3)
+vCharacter <- c('a','b','c')
+vLogical <- c(T,F,T)
+
+dfa <- cbind(vNumeric,vCharacter,vLogical)
+df= as.data.frame(dfa)
+
+### sequence generation
+x1= seq(10)
+print(x1)
+
+(x2= seq(30,0, by=-3))
+
+
+######### Importing data
+data<- read.csv('/home/kd/Downloads/AIS_2025_code_session/example_data.csv')
+
+######## Linear regression
+?USJudgeRatings
+head(USJudgeRatings)
+data <- USJudgeRatings
+
+reg1= lm(RTEN~., data)
+summary(reg1)
